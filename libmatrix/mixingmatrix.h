36f8fbd6ef662b6ec638784dd0cd58539531d19b
Boyue_Qu <446887402@qq.com>
2021-07-26 00:12:37 +0100

    debug

--- libmatrix/mixingmatrix.h
+++ libmatrix/mixingmatrix.h
@@ -263,7 +263,7 @@ protected:
 
 protected slots:
 	// Use this slot if you don't want to do something before replacement.
-	virtual void slot_simple_replace() { qDebug() << "		--slot_simple_replace"; replace(this, type); }
+	virtual void slot_simple_replace() { qDebug() << "		--slot_simple_replace"; QString type = ""; replace(this, type); }
 	virtual void slot_simple_replace(QString type) { qDebug() << "		--slot_simple_replace_with type  "<<type; replace(this,type); }
 	// Use this slot if you don't want to do something before explosion.
 	virtual void slot_simple_explode() { explode( this ); }
